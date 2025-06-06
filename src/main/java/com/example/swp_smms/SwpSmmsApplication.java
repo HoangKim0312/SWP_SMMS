b9d31731f6b2cf229986a62b09a8a4292dc5b9d8
VolDeBug <hiepvl987654321@gmail.com>
2025-06-06 10:11:31 +0700

    Update SwpSmmsApplication.java

--- src/main/java/com/example/swp_smms/SwpSmmsApplication.java
+++ src/main/java/com/example/swp_smms/SwpSmmsApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SwpSmmsApplication {
-    //test
+    //test by hiep
     public static void main(String[] args) {
         SpringApplication.run(SwpSmmsApplication.class, args);
     }
