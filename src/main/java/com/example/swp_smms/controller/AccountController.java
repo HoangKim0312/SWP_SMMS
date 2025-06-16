5ae1b2dd2071dc93d2e7cf8efe1635daaf63c1af
Hoàng Lưu <hoangluu5555@gmail.com>
2025-06-16 21:37:13 +0700

    adjust message success for changePassword

--- src/main/java/com/example/swp_smms/controller/AccountController.java
+++ src/main/java/com/example/swp_smms/controller/AccountController.java
@@ -1,36 +1,22 @@
 package com.example.swp_smms.controller;
 
-import com.example.swp_smms.model.entity.Account;
 import com.example.swp_smms.model.payload.request.AccountRequest;
 import com.example.swp_smms.model.payload.response.AccountResponse;
 import com.example.swp_smms.service.AccountService;
-import com.example.swp_smms.model.payload.request.ChangePasswordRequest;
 import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.UUID;
-
 @RestController
-@RequestMapping("/api/accounts")
+@RequestMapping("/api/v1/accounts")
+@RequiredArgsConstructor
 public class AccountController {
 
-    @Autowired
-    private AccountService accountService;
+    private final AccountService accountService;
 
-    @PostMapping("/create")
-    @ResponseStatus(HttpStatus.CREATED)
-    public AccountResponse createAccount(@RequestBody AccountRequest accountRequest) {
-        return accountService.createAccount(accountRequest);
-    }
-
-    @PutMapping("/{accountId}/change-password")
-    public ResponseEntity<Void> changePassword(
-            @PathVariable UUID accountId,
-            @Valid @RequestBody ChangePasswordRequest request) {
-        accountService.changePassword(accountId, request);
-        return ResponseEntity.ok().build();
+    @PostMapping
+    public ResponseEntity<AccountResponse> createAccount(@Valid @RequestBody AccountRequest accountRequest) {
+        return ResponseEntity.ok(accountService.createAccount(accountRequest));
     }
 }
