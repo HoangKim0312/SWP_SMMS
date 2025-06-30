51d2b324d146c7f36b605e97d437e4247ff115eb
HoangKim0312 <hoangluu5555@gmail.com>
2025-06-30 18:39:03 +0700

    add getStudent for parent api

---
+++ src/main/java/com/example/swp_smms/model/payload/response/StudentSummaryResponse.java
@@ -0,0 +1,9 @@
+package com.example.swp_smms.model.payload.response;
+
+import lombok.Data;
+
+@Data
+public class StudentSummaryResponse {
+    private String fullName;
+    private String className;
+} 
