97d7d464411e32703c1fdb3c971738753b27baf9
Acs05 <vtoan1101@gmail.com>
2025-07-06 12:29:48 +0700

    Fixed healthcheck APIs

--- src/main/java/com/example/swp_smms/model/payload/response/VaccinationConfirmationResponse.java
+++ src/main/java/com/example/swp_smms/model/payload/response/VaccinationConfirmationResponse.java
@@ -2,11 +2,15 @@ package com.example.swp_smms.model.payload.response;
 
 import lombok.Data;
 import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 import java.util.UUID;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class VaccinationConfirmationResponse {
     private Long confirmationId;
     private Long vaccineNoticeId;
