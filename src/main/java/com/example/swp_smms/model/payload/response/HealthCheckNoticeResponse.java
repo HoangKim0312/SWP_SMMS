da9739858185887a1da38bd146b1c21475dd11cc
Acs05 <vtoan1101@gmail.com>
2025-07-06 12:29:48 +0700

    Fixed healthcheck APIs

--- src/main/java/com/example/swp_smms/model/payload/response/HealthCheckNoticeResponse.java
+++ src/main/java/com/example/swp_smms/model/payload/response/HealthCheckNoticeResponse.java
@@ -2,9 +2,13 @@ package com.example.swp_smms.model.payload.response;
 
 import lombok.Data;
 import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class HealthCheckNoticeResponse {
     private Long checkNoticeId;
     private String title;
